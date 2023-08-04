690bd1c4ac2233aeb501b1cb0e96e5874a59c86f
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-04 19:03:11 +0000

    Links to detailed url updated in MenuDropdown list

--- src/components/MenuDropdown.js
+++ src/components/MenuDropdown.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
 import '../styles.css';
 
 const DropdownMenu = ({ menuData }) => {
@@ -19,21 +20,17 @@ const DropdownMenu = ({ menuData }) => {
       onMouseEnter={handleMenuEnter}
       onMouseLeave={handleMenuLeave}
     >
-      <a
-        href="#"
-        className="navbar-text nav-link fw-bold"
-        role="button"
-        onClick={(e) => e.preventDefault()} // Prevent the link from navigating
-      >
+      <Link to="/" className="navbar-text nav-link fw-bold"> {/* Use the Link component instead of <a> */}
         <FaHome className="me-1" />
         Menu
-      </a>
+      </Link>
       <div className={`menu-items ${isOpen ? 'show' : ''}`}>
         {menuData.map((menuItem, index) => (
           <div key={index}>
-            <a href={menuItem.href} className="menu-item" id='dropdownLinks'>
+            {/* Use the Link component with the correct URL */}
+            <Link to={`/menu/${encodeURIComponent(menuItem.dropdownTitle)}`} className="menu-item" id='dropdownLinks'>
               {menuItem.dropdownTitle}
-            </a>
+            </Link>
           </div>
         ))}
       </div>
