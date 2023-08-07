0cca75dabecf76c8f868832a2817d6e09196af35
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-07 16:09:04 +0000

    MenuItemSummaryTemplate styled and next is to collect menu data

--- src/components/MenuItemSummaryTemplate.js
+++ src/components/MenuItemSummaryTemplate.js
@@ -4,15 +4,29 @@ import '../styles/MenuItemSummaryTemplate.css';
 
 const MenuItemSummaryTemplate = () => {
   return (
-    <div className="MenuItemSummary-container">
-      <div className="MenuItemSummary-header">
+    <div className="MenuItemSummary-container container-fluid">
+      <div className="MenuItemSummary-header text-center container-fluid">
         <h1>Menu Summary!</h1><hr />
       </div>
-      {/* Display selected items */}
-      {/* ... */}
+      <div className="MenuItemSummary-instructions text-center container-fluid">
+        <p>Welcome to the menu summary, where you can view your items and delete unwanted items from the menu!<br />
+        To delete items from your Cart, just select any items in the list and press the <strong><em>"Delete"</em></strong> button at the bottom of this page...<br />
+        If you are saticfied with your menu, then note down your summay and had back to to the menu page by clicking on the <strong><em>"Back To Menu"</em></strong> button.<br />
+        When we have added an online payment method, you will be able to "Checkout" your items and reserve your dinner in advanced together with your booking.<br />
+        <strong>Enjoy you meal!</strong>
+        </p>
+      </div>
+      <div className="MenuItemSummary-items">
+        {/* Display selected items */}
+        {/* ... */}
+      </div>
       <div className="MenuSummary-button-wrapper">
-        <button className="MenuSummary-button" id='MenuSummary-Delete'>Delete Items</button>
-        <button className="MenuSummary-button" id='MenuSummary-Home'>Back To Menu</button>
+        <button className="MenuSummary-button" id="MenuSummary-Delete">
+          Delete Items
+        </button>
+        <button className="MenuSummary-button" id="MenuSummary-Home">
+          Back To Menu
+        </button>
       </div>
     </div>
   );
