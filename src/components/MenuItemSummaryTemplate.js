e0d0e6da5e2ed28dd1f3eab10cc45c03d7fed93a
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-08-12 21:58:24 +0000

    MenuItemSummaryTemplate styled with css

--- src/components/MenuItemSummaryTemplate.js
+++ src/components/MenuItemSummaryTemplate.js
@@ -51,26 +51,45 @@ const MenuItemSummaryTemplate = () => {
       </div>
       <div className="MenuItemSummary-instructions text-center container-fluid">
         <p>Welcome to the menu summary, where you can view your items and delete unwanted items from the menu!<br />
-        To delete items from your Cart, just select any items in the list and press the <strong><em>"Delete"</em></strong> button at the bottom of this page...<br />
-        If you are saticfied with your menu, then note down your summay and had back to to the menu page by clicking on the <strong><em>"Back To Menu"</em></strong> button.<br />
+        To change the items in your Cart, just select any items from the list and then press the <strong><em>"Delete"</em></strong> OR <strong><em>"Update"</em></strong> button at the bottom of this page.<br />
+        If you are saticfied with your menu, then note down your summay and had back to to the menu page by clicking on the <strong><em>"Confirm Order"</em></strong> button.<br />
         When we have added an online payment method, you will be able to "Checkout" your items and reserve your dinner in advanced together with your booking.<br />
         <strong>Enjoy you meal!</strong>
         </p>
       </div>
-      <div className="MenuItemSummary-items">
+      <div className="MenuItemSummary-items container-fluid">
         {/* Display selected items */}
-        <ul>
+        <ol>
           {console.log('cartItems:', cartItems)}
           {cartItems.map((item, index) => (
-           <li key={index}>
-         {item.title} - {item.name} - Price: ${item.price} - Quantity: {item.quantity}
-           </li>
-))}
-        </ul>
+            <li key={index}>
+              {item.title}: {item.name}<br/>
+              {item.description}<br/>
+              Price: ${item.price} -- Quantity: {item.quantity} -- Select-item <input type="checkbox" />
+            </li>
+          ))}
+          
+          {/* Duplicate the list items with the same parameters */}
+          {cartItems.map((item, index) => (
+            <li key={index + cartItems.length}>
+              {item.title}: {item.name}<br/>
+              {item.description}<br/>
+              Price: ${item.price} -- Quantity: {item.quantity} -- Select-item <input type="checkbox" />
+            </li>
+          ))}
+        </ol>
       </div>
-      <div className='MenuSummary-button-wrapper'>
+      <div className="MenuSummary-button-wrapper-update">
+        <div className="MenuItemSummary-total">
+          Total:
+        </div>
+        <button className="MenuSummary-button" id="MenuSummary-Update">
+          Update
+        </button>
+      </div>
+      <div className='MenuSummary-button-wrapper-delete-confirm'>
         <button className="MenuSummary-button" id="MenuSummary-Home" onClick={handleConfirm}>
-          Confirm
+          Confirm Order
         </button>
         <button className="MenuSummary-button" id="MenuSummary-Delete">
           Delete Items
@@ -81,7 +100,7 @@ const MenuItemSummaryTemplate = () => {
       <Modal.Title>Confirm Action</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-         Are you sure you want to confirm this action?
+         Are you sure that you are saticfied with you menu?
       </Modal.Body>
       <Modal.Footer>
       <Button variant="secondary" onClick={handleCloseModal}>
